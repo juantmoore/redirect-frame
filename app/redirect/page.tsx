2495b396c01a4bb6402ed3a805cbf49b16e59eaf
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-01 13:23:57 -0600

    change link to commerce

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,22 +1,22 @@
 'use client';
-import {useRouter} from "next/navigation";
-import {useEffect} from "react";
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function RedirectPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        const youtubeUrl = 'https://www.youtube.com/watch?v=MwCn50ZVPGQ&feature=youtu.be';
+  useEffect(() => {
+    const hostedUrl = 'https://commerce.coinbase.com/pay/db2affb4-b2c9-4b2b-b7f8-e89b8bbc69ac';
 
-        // Perform the redirect
-        window.location.href = youtubeUrl; // For a full page reload redirect
-        // Or use Next.js router for client-side redirect (comment out the line above if using this)
-        // router.push(youtubeUrl);
-    }, [router]);
+    // Perform the redirect
+    window.location.href = hostedUrl; // For a full page reload redirect
+    // Or use Next.js router for client-side redirect (comment out the line above if using this)
+    // router.push(youtubeUrl);
+  }, [router]);
 
-    return (
-        <div>
-            <p>Redirecting...</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>Redirecting...</p>
+    </div>
+  );
 }
