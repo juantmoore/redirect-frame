1af56af666d3ae82168bf3acbdffbbc9fbb5a69f
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-02 13:01:59 -0600

    add dynamic checkout

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,18 +1,31 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { GetServerSideProps } from 'next';
+const NEXT_PUBLIC_URL = 'https://commerce-frame-6r9h.vercel.app'; //replace me
+// interface RedirectPageProps {
+//   hostedUrl: string;
+// }
 
 export default function RedirectPage() {
-  const router = useRouter();
-
   useEffect(() => {
-    const hostedUrl = 'https://commerce.coinbase.com/pay/db2affb4-b2c9-4b2b-b7f8-e89b8bbc69ac';
+    async function fetchHostedUrl() {
+      try {
+        const response = await fetch('/api/frame', { method: 'POST' });
+        const data = await response.json();
 
-    // Perform the redirect
-    window.location.href = hostedUrl; // For a full page reload redirect
-    // Or use Next.js router for client-side redirect (comment out the line above if using this)
-    // router.push(youtubeUrl);
-  }, [router]);
+        if (data.hostedUrl) {
+          window.location.href = data.hostedUrl;
+        } else {
+          console.error('Hosted URL not found');
+        }
+      } catch (error) {
+        console.error('Error fetching hosted URL:', error);
+      }
+    }
+
+    fetchHostedUrl();
+  }, []);
 
   return (
     <div>
