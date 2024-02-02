9e881325696e34c3228ef4a1d8381656c459680e
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-02 14:38:32 -0600

    redo redirect

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,39 +1,3 @@
-// 'use client';
-// import { useRouter } from 'next/navigation';
-// import { useEffect } from 'react';
-// import { GetServerSideProps } from 'next';
-// const NEXT_PUBLIC_URL = 'https://commerce-frame-6r9h.vercel.app'; //replace me
-// // interface RedirectPageProps {
-// //   hostedUrl: string;
-// // }
-
-// export default function RedirectPage() {
-//   useEffect(() => {
-//     async function fetchHostedUrl() {
-//       try {
-//         const response = await fetch('/api/frame', { method: 'POST' });
-//         const data = await response.json();
-
-//         if (data.hostedUrl) {
-//           window.location.href = data.hostedUrl;
-//         } else {
-//           console.error('Hosted URL not found');
-//         }
-//       } catch (error) {
-//         console.error('Error fetching hosted URL:', error);
-//       }
-//     }
-
-//     fetchHostedUrl();
-//   }, []);
-
-//   return (
-//     <div>
-//       <p>Redirecting...</p>
-//     </div>
-//   );
-// }
-
 'use client';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
