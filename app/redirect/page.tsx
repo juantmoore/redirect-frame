75b15d35b3a96a2849b546df04bb790c06bc13aa
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-02 20:32:42 -0600

    revert

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,17 +1,31 @@
 'use client';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { GetServerSideProps } from 'next';
+const NEXT_PUBLIC_URL = 'https://commerce-frame-6r9h.vercel.app'; //replace me
+
+//this is the file i would like to take the `id` passed as a query paramter, construct a URL in this format:
+// https://commerce.coinbase.com/pay/${id} then redirect the user to that url
 
 export default function RedirectPage() {
-  const router = useRouter();
-  const { id } = router.query; // Get the 'id' query parameter
-
   useEffect(() => {
-    if (id) {
-      const hostedUrl = `https://commerce.coinbase.com/pay/${id}`;
-      window.location.href = hostedUrl;
+    async function fetchHostedUrl() {
+      try {
+        const response = await fetch('/api/frame', { method: 'POST' });
+        const data = await response.json();
+
+        if (data.hostedUrl) {
+          window.location.href = data.hostedUrl;
+        } else {
+          console.error('Hosted URL not found');
+        }
+      } catch (error) {
+        console.error('Error fetching hosted URL:', error);
+      }
     }
-  }, [id]); // Depend on 'id' to trigger effect
+
+    fetchHostedUrl();
+  }, []);
 
   return (
     <div>
