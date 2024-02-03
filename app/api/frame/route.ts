6b38deeacd0c8f9f965db65ba1eb4e88697dc0ba
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-02 20:32:30 -0600

    handle charge creation and redirect

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -68,49 +68,22 @@ async function createCharge(chargeData: ChargeRequestBody): Promise<any> {
   }
 }
 
-async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const chargeResponse = await createCharge(requestBody);
-  const chargeId = chargeResponse.data.id; // Assuming 'id' is the correct field from the response
-
-  return NextResponse.redirect(`${NEXT_PUBLIC_URL}/redirect?id=${chargeId}`, { status: 302 });
+async function getResponse(req: NextRequest, hostedUrl: string): Promise<NextResponse> {
+  return NextResponse.redirect(hostedUrl, { status: 302 });
 }
 
-// export async function POST(req: NextRequest): Promise<NextResponse> {
-//   try {
-//     const responseData = await createCharge(requestBody);
-//     const hostedUrl = responseData.data.hosted_url;
-//     console.log('hostedUrl: ', hostedUrl);
-//     return getResponse(hostedUrl); // Use the hostedUrl for redirection
-//   } catch (error) {
-//     console.error('Error in POST function:', error);
-//     return new NextResponse(JSON.stringify({ error: 'Failed to create charge' }), {
-//       status: 500,
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-//   }
-// }
-
-// async function getResponse(req: NextRequest): Promise<NextResponse> {
-//   let accountAddress: string | undefined = '';
-
-//   const body: FrameRequest = await req.json();
-//   const { isValid, message } = await getFrameMessage(body);
-
-//   if (isValid) {
-//     try {
-//       accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-
-//   console.log('getResponse()');
-//   return NextResponse.redirect(`${NEXT_PUBLIC_URL}/redirect`, { status: 302 });
-// }
-
-export async function POST(req: NextRequest): Promise<Response> {
-  console.log('POST');
-  return getResponse(req);
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  try {
+    const responseData = await createCharge(requestBody);
+    const hostedUrl = responseData.data.hosted_url; // Get the hosted URL from the response
+    return getResponse(req, hostedUrl); // Redirect to the hosted URL
+  } catch (error) {
+    console.error('Error in POST function:', error);
+    return new NextResponse(JSON.stringify({ error: 'Failed to create charge' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
 }
 
 export const dynamic = 'force-dynamic';
