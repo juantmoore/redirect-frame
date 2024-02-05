cbd234fca0756136443c5a3b1afc88c976d3d96e
Patrick Hughes <patrick.hughes@coinbase.com>
2024-02-05 11:22:56 -0600

    use variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL, IMAGE_NAME, ITEM_DESCRIPTION, ITEM_TITLE } from '../utils/utils';
 
-const NEXT_PUBLIC_URL = 'https://commerce-frame-6r9h.vercel.app';
 const frameMetadata = getFrameMetadata({
   buttons: [{ label: 'Buy', action: 'post_redirect' }],
-  image: `${NEXT_PUBLIC_URL}/onchain.png`,
+  image: `${NEXT_PUBLIC_URL}/${IMAGE_NAME}`,
   post_url: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
-  title: 'Base Beanies',
-  description: 'Exclusive Base beanies for Warpcaster users',
+  title: ITEM_TITLE,
+  description: ITEM_DESCRIPTION,
   openGraph: {
-    title: 'Base Beanies',
-    description: 'Exclusive Base beanies for Warpcaster users',
+    title: ITEM_TITLE,
+    description: ITEM_DESCRIPTION,
     images: [NEXT_PUBLIC_URL],
   },
   other: {
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <img src={'/onchain.png'} />
+      <img src={`/${IMAGE_NAME}}`} />
     </>
   );
 }
