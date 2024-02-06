5e360c8d266f2888fa0de549be39ae333ef0ea7d
Tony Hughes <tonyh@tonys-air.lan>
2024-02-05 22:11:49 -0600

    resort back to normal

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,32 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import { NEXT_PUBLIC_URL, IMAGE_NAME, ITEM_DESCRIPTION, ITEM_TITLE } from '../utils/utils';
 
+// const frameMetadata = getFrameMetadata({
+//   buttons: [{ label: 'Buy', action: 'post_redirect' }],
+//   image: `${NEXT_PUBLIC_URL}/${IMAGE_NAME}`,
+//   post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+// });
+
+// export const metadata: Metadata = {
+//   title: ITEM_TITLE,
+//   description: ITEM_DESCRIPTION,
+//   openGraph: {
+//     title: ITEM_TITLE,
+//     description: ITEM_DESCRIPTION,
+//     images: [NEXT_PUBLIC_URL],
+//   },
+//   other: {
+//     ...frameMetadata,
+//   },
+// };
+
 const frameMetadata = getFrameMetadata({
-  buttons: [{ label: 'Buy', action: 'post_redirect' }],
-  image: `${NEXT_PUBLIC_URL}/${IMAGE_NAME}`,
-  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+  buttons: [
+      {label: 'Click to unlock', action: 'post_redirect'}
+  ],
+  image: 'https://redirect-frame-tau.vercel.app/onchain.png',
+  post_url: 'https://redirect-frame-tau.vercel.app/api/frame',
 });
-
-export const metadata: Metadata = {
-  title: ITEM_TITLE,
-  description: ITEM_DESCRIPTION,
-  openGraph: {
-    title: ITEM_TITLE,
-    description: ITEM_DESCRIPTION,
-    images: [NEXT_PUBLIC_URL],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
-
-
 
 export default function Page() {
   console.log(process.env.API_VERSION)
